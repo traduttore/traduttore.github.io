67e0629d23af40252e9a1463ea9a2ed3ac39b455
Alex Barkin <alexbarkin@AlexsMacBookPro.local>
2019-10-10 22:20:41 -0400

    first launch page

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,43 @@
  */
 
 module.exports = {
-  /* Your site config here */
+  siteMetadata: {
+    title: `Alex's Website`,
+    siteUrl: `localhost:8000`,
+    description: `My own personal website, all about me!`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        "fonts": [
+          {
+            "family": "Roboto",
+            "variants": [
+              "400",
+              "400i",
+              "700",
+              "700i"
+            ],
+            "subsets": [
+              "latin-ext"
+            ]
+          }
+        ],
+        "formats": [
+          "woff",
+          "woff2"
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/images`,
+        name: 'images'
+      }
+    },
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+  ]
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,28 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+import Img from 'gatsby-image';
+import { Link, graphql } from 'gatsby';
+
+import '../styles/index.css'
+
+const IndexPage = props => (
+<body>
+    <h1 className='index--title'>Alex's Website</h1>
+    <Img className="index--image" fixed={props.data.launchImage.childImageSharp.fixed} alt="" />
+    <Link className='index--launch' to='/about/'>Launch.</Link>
+</body>
+)
+
+export default IndexPage;
+
+export const query = graphql`
+  query {
+    launchImage: file(relativePath: { eq: "launchImage.png" }) {
+      childImageSharp {
+        fixed(width: 500, quality: 100) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+  }
+`
