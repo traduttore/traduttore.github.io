2fdf08a27ada8b6ba66b20dc6d74cc6b1156e704
Alex Barkin <alexbarkin@AlexsMacBookPro.local>
2019-10-23 20:57:49 -0400

    gatsby working branch

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,10 +7,40 @@
 module.exports = {
   siteMetadata: {
     title: `Alex's Website`,
+    menuLinks:[
+        {
+          name:'home',
+          link:'/'
+        },
+        {
+          name:'about',
+          link:'/About'
+        },
+        {
+          name:'hello',
+          link:'/blog/hello'
+        }
+      ],
     siteUrl: `localhost:8000`,
     description: `My own personal website, all about me!`,
   },
   plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options:
+        {
+          path: `${__dirname}/src/images`,
+          name: 'images'
+        }
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options:
+        {
+          path: `${__dirname}/src/markdown-pages`,
+          name: 'markdown-pages'
+        }
+    },
     {
       resolve: `gatsby-plugin-prefetch-google-fonts`,
       options: {
@@ -34,14 +64,9 @@ module.exports = {
         ],
       },
     },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/src/images`,
-        name: 'images'
-      }
-    },
     'gatsby-transformer-sharp',
+    `gatsby-transformer-remark`,
+    `gatsby-plugin-react-helmet`,
     'gatsby-plugin-sharp',
   ]
 }
