071b50a86cd024f0aa8a3b9eabfac5d904aa14c3
Alex Barkin <alexbarkin@AlexsMacBookPro.local>
2019-10-25 11:28:24 -0400

    safe Version

---
+++ src/components/CallingCard.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+
+import headshot from "../images/headshot.jpg"
+import '../styles/app.css';
+
+const Callingcard = props => (
+    <div className="container--address">
+        <address className="container--address-me">
+            <img src={headshot} className="headshot" alt="headshot"/>
+            <p className="container--address-text">
+                    Alex Barkin<br/> 
+                    Linkedin: <a href="https://www.linkedin.com/in/abarkin/">abarkin</a><br/>
+                    Github: <a href="https://github.com/alexbarkin">alexbarkin</a><br/>
+                    Email: <a href="mailto:alex.barkin@edu.uwaterloo.ca">alex.barkin@edu.uwaterloo.ca</a><br/>
+            </p>
+        </address>
+        <iframe src="https://snapwidget.com/embed/751863" class="snapwidget-widget" allowtransparency="true" frameborder="0" scrolling="no" className="instagram" title="instagram"></iframe>
+    </div>
+)
+
+export default Callingcard;
---
+++ src/pages/Blog.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+
+import { Link } from 'gatsby';
+
+import Layout from '../components/Layout.jsx'
+
+import '../styles/app.css';
+
+export default () => (
+    <Layout>
+        <h1>Blogs</h1>
+        <nav>
+            <a href="/blog/hello">hello blog</a><br/>
+            <a href="/blog/file-2">another blog</a>
+        </nav>
+        <Link to='/About'>Learn more about me!</Link>
+    </Layout>
+)
