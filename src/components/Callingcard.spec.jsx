0c425edbc764292f3d4d4cf565f9b16993f393c1
Alex Barkin <alexbarkin@rogers.com>
2019-11-25 10:21:34 -0500

    styling

--- src/components/Callingcard.spec.jsx
+++ src/components/Callingcard.spec.jsx
@@ -5,22 +5,22 @@ import Callingcard from "./Callingcard.jsx"
 
 describe("Calling Card", () => {
   it("renders", () => {
-    const sut = shallow(<Callingcard />);
-    expect(sut.find('.container--address').exists()).toBeTruthy();
-  });
+    const sut = shallow(<Callingcard />)
+    expect(sut.find(".container--address").exists()).toBeTruthy()
+  })
 
   it("renders with an image", () => {
-    const sut = shallow(<Callingcard />);
-    expect(sut.find("img").exists()).toBeTruthy();
-  });
+    const sut = shallow(<Callingcard />)
+    expect(sut.find("img").exists()).toBeTruthy()
+  })
 
   it("renders with external content in the form of an iframe", () => {
-    const sut = shallow(<Callingcard />);
-    expect(sut.find("iframe").exists()).toBeTruthy();
-  });
+    const sut = shallow(<Callingcard />)
+    expect(sut.find("iframe").exists()).toBeTruthy()
+  })
 
   it("renders three links in the address section", () => {
-    const sut = shallow(<Callingcard />);
-    expect(sut.find("address").find('a').length).toBe(3);
-  });
-});
+    const sut = shallow(<Callingcard />)
+    expect(sut.find("address").find("a").length).toBe(3)
+  })
+})
--- src/pages/Resume.spec.jsx
+++ src/pages/Resume.spec.jsx
@@ -5,9 +5,9 @@ import Resume from "./Resume.jsx"
 
 describe("Resume", () => {
   it.only("renders with a title and pdf", () => {
-    const sut = shallow(<Resume />);
-    console.log(sut.debug());
-    expect(sut.find('h1').exists()).toBeTruthy();
-    expect(sut.find('embed').exists()).toBeTruthy();
-  });
-});
+    const sut = shallow(<Resume />)
+    console.log(sut.debug())
+    expect(sut.find("h1").exists()).toBeTruthy()
+    expect(sut.find("embed").exists()).toBeTruthy()
+  })
+})
