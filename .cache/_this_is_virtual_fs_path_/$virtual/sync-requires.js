3c5d08cb8b247cb0e8a622738b51c914ba7d1f7d
Alex <alexbarkin@rogers.com>
2022-01-14 17:06:57 -0500

    Personalization

--- .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -4,7 +4,6 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": preferDefault(require("/Users/alexbarkin/Desktop/Waterloo/4B/FYDP/traduttore.github.io/.cache/dev-404-page.js")),
   "component---src-pages-404-js": preferDefault(require("/Users/alexbarkin/Desktop/Waterloo/4B/FYDP/traduttore.github.io/src/pages/404.js")),
   "component---src-pages-index-js": preferDefault(require("/Users/alexbarkin/Desktop/Waterloo/4B/FYDP/traduttore.github.io/src/pages/index.js")),
   "component---src-pages-week-1-js": preferDefault(require("/Users/alexbarkin/Desktop/Waterloo/4B/FYDP/traduttore.github.io/src/pages/week1.js"))
--- .cache/page-ssr/routes/component---src-pages-week-1-js.js
+++ .cache/page-ssr/routes/component---src-pages-week-1-js.js
@@ -1,22 +1,18 @@
 "use strict";
-exports.id = "component---src-pages-week-1-js";
-exports.ids = ["component---src-pages-week-1-js"];
+exports.id = 714;
+exports.ids = [714];
 exports.modules = {
 
-/***/ "./src/pages/week1.js":
-/*!****************************!*\
-  !*** ./src/pages/week1.js ***!
-  \****************************/
+/***/ 691:
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
 __webpack_require__.r(__webpack_exports__);
-/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "react");
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(195);
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
-
-
-const blog1 = () => {
-  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("h1", null, "Blog 1");
-};
+const blog1=()=>{return/*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("h1",null,"Blog 1");};/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (blog1);
 
 /***/ })
 
