0a37dd41e707078e33220bfa37e1e12335b982ec
Alex <alexbarkin@rogers.com>
2022-01-14 16:04:43 -0500

    Personalization

---
+++ .cache/page-ssr/routes/component---src-pages-week-1-js.js
@@ -0,0 +1,25 @@
+"use strict";
+exports.id = "component---src-pages-week-1-js";
+exports.ids = ["component---src-pages-week-1-js"];
+exports.modules = {
+
+/***/ "./src/pages/week1.js":
+/*!****************************!*\
+  !*** ./src/pages/week1.js ***!
+  \****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "react");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+
+
+const blog1 = () => {
+  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("h1", null, "Blog 1");
+};
+
+/***/ })
+
+};
+;
+//# sourceMappingURL=component---src-pages-week-1-js.js.map
---
+++ src/pages/week1.js
@@ -0,0 +1,7 @@
+import * as React from "react"
+
+const blog1 = () => {
+    return (<h1>Blog 1</h1>)
+}
+
+export default blog1
