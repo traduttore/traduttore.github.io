dff336e3ccfa1c89eb6100c97be1e042dec64a54
TheCamNelson <ccnelson@edu.uwaterloo.ca>
2022-01-17 17:19:21 -0500

    initial commit

--- .cache/page-ssr/routes/component---src-pages-week-1-js.js
+++ .cache/page-ssr/routes/component---src-pages-week-1-js.js
@@ -1,18 +1,27 @@
 "use strict";
-exports.id = 714;
-exports.ids = [714];
+exports.id = "component---src-pages-week-1-js";
+exports.ids = ["component---src-pages-week-1-js"];
 exports.modules = {
 
-/***/ 691:
+/***/ "./src/pages/week1.js":
+/*!****************************!*\
+  !*** ./src/pages/week1.js ***!
+  \****************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
 __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
-/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(195);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "react");
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
-const blog1=()=>{return/*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("h1",null,"Blog 1");};/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (blog1);
+
+
+const blog1 = () => {
+  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("h1", null, "Blog 1");
+};
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (blog1);
 
 /***/ })
 
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -4,8 +4,10 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---src-pages-404-js": preferDefault(require("/Users/alexbarkin/Desktop/Waterloo/4B/FYDP/traduttore.github.io/src/pages/404.js")),
-  "component---src-pages-index-js": preferDefault(require("/Users/alexbarkin/Desktop/Waterloo/4B/FYDP/traduttore.github.io/src/pages/index.js")),
-  "component---src-pages-week-1-js": preferDefault(require("/Users/alexbarkin/Desktop/Waterloo/4B/FYDP/traduttore.github.io/src/pages/week1.js"))
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/camnelson/Desktop/traduttore.github.io/.cache/dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("/Users/camnelson/Desktop/traduttore.github.io/src/pages/404.js")),
+  "component---src-pages-home-js": preferDefault(require("/Users/camnelson/Desktop/traduttore.github.io/src/pages/home.js")),
+  "component---src-pages-index-js": preferDefault(require("/Users/camnelson/Desktop/traduttore.github.io/src/pages/index.js")),
+  "component---src-pages-week-1-js": preferDefault(require("/Users/camnelson/Desktop/traduttore.github.io/src/pages/week1.js"))
 }
 
